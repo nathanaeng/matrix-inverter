b63753a4b0f410ddfd107835b01f7e2f4eebd117
nathanaeng <nathaneng89@gmail.com>
2022-01-21 21:06:35 -0800

    Expanded test suite to cover some edge cases

--- test_matrix_inverter.py
+++ test_matrix_inverter.py
@@ -1,5 +1,5 @@
 import unittest
-from matrix_inverter import Matrix
+from matrix import Matrix
 
 class TestMatrixConstructor(unittest.TestCase):
 
@@ -44,6 +44,11 @@ class TestDeterminant(unittest.TestCase):
         m = Matrix(data)
         self.assertEqual(False, m.has_inverse())
 
+    def test_get_det_0x0(self):
+        m = Matrix([[]])
+        self.assertEqual(True, m.has_inverse())
+        self.assertEqual(m.get_inverse().data, [[]])
+
 
 class TestTranspose(unittest.TestCase):
 
@@ -53,6 +58,14 @@ class TestTranspose(unittest.TestCase):
         transposed = [[0,0,1],[4,0,2],[3,4,3]]
         self.assertEqual(transposed, m.transpose().data)
 
+    def test_transpose_1x1(self):
+        m = Matrix([[1]])
+        self.assertEqual([[1]], m.transpose().data)
+
+    def test_transpose_0x0(self):
+        m = Matrix([[]])
+        self.assertEqual([[]], m.transpose().data)
+
 
 class TestInverse(unittest.TestCase):
 
@@ -60,6 +73,25 @@ class TestInverse(unittest.TestCase):
         data = [[0,3,1],[2,0,0],[1,-2,1]]
         m = Matrix(data)
         inverse_m = [[0, 0.5, 0], [0.2, 0.1, -0.2], [0.4, -.3, 0.6]]
+        self.assertEqual(inverse_m, m.get_inverse().data)
+
+    def test_inverse_1x1(self):
+        data = [[2]]
+        m = Matrix(data)
+        inverse_m = [[0.5]]
+        self.assertEqual(inverse_m, m.get_inverse().data)
+
+    def test_inverse_invalid_3x3(self):
+        data = [[0,0,0],[2,4,0],[7,5,1]]
+        m = Matrix(data)
+
+        with self.assertRaises(Exception):
+             m.get_inverse().data
+        self.assertTrue("Matrix is not invertible")
+
+    def test_inverse_0x0(self):
+        m = Matrix([[]])
+        inverse_m = [[]]
         self.assertEqual(inverse_m, m.get_inverse().data)
 
 
